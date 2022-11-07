7b13c8492b46bf3dfdfa26ed1d49ffb1e2875361
G3nHawkeye <113044132+michalgvpt@users.noreply.github.com>
2022-11-07 21:57:04 +0100

    Update 1.uloha.py

--- ciphers/1.uloha.py
+++ ciphers/1.uloha.py
@@ -10,8 +10,8 @@ def encode(text:str,key:str)->str:
 def decode(text:str,key:str)->str:
     empty=''
     for i in range(len(text)):
-        move=ord(key[i%len(key)])-96
-        txt=(ord(text[i])-97-move)%26+97
+        change=ord(key[i%len(key)])-96
+        txt=(ord(text[i])-97-change)%26+97
         ky=chr(txt)
         empty+=ky
     return empty
